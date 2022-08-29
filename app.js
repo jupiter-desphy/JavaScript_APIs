aff1da2c57769fe4577bc24248ebd7e7d3666bdb
Jupiter Desphy <jupiterdesphy7@gmail.com>
2022-08-29 14:05:31 -0700

    complete

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
+"use strict"
+
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let searchButton = document.querySelector("#submitSearch");
+let searchInput = document.querySelector("#searchWord");
+let image = document.getElementById("gif")
+let feedback = document.querySelector("#feedback")
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "8xT89uK8gxhNC8CZx61XKOfdkpCFmAOa";
+
+searchButton.addEventListener("click", (event) => {
+    console.log (event);
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&S=${searchInput}`)
+    .then((res) => {
+        return res.json();
+    })
+    .then((resJSON) => {
+        console.log(resJSON);
+        image.src = resJSON.data.images.original.url;
+        image.alt = resJSON.data.title;
+        searchInput.value = ""
+        feedback.textContent = "";``
+    })
+    .catch((err) => {
+        console.error(err);
+        feedback.textContent = err.message;
+    });
+});
