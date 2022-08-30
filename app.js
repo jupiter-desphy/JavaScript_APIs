0937b0a972accbc256e17516673a62a5f3952491
Jupiter Desphy <jupiterdesphy7@gmail.com>
2022-08-30 11:33:38 -0700

    corrected input to include .value

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ console.log("EXERCISE 1:\n==========\n");
 
 let searchButton = document.querySelector("#submitSearch");
 let searchInput = document.querySelector("#searchWord");
-let image = document.getElementById("gif")
-let feedback = document.querySelector("#feedback")
+let image = document.getElementById("gif");
+let feedback = document.querySelector("#feedback");
+let form = document.querySelector("#inputContainer");
 
 const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
 const GIPHY_KEY = "8xT89uK8gxhNC8CZx61XKOfdkpCFmAOa";
 
 searchButton.addEventListener("click", (event) => {
     console.log (event);
-    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&S=${searchInput}`,
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&S=${searchInput.value}`,
     {mode: "cors"}
     )
         .then((res) => {
